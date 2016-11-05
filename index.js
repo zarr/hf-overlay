f72c541d62c161cd9c89e0fbbc66260df7c89a21
Juho <juho.jokelainen@vincit.fi>
2016-11-05 16:59:08 +0200

    Add socket.io

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var express = require('express')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
-app.listen(3000, function () {
+io.on('connection', function(socket) {
+  console.log('a user connected');
+  setInterval(function() {
+    console.log("Sending toggle")
+    socket.emit('toggle');
+  }, 500 );
+})
+
+http.listen(3000, function () {
   console.log('Server started!')
 })
 
 app.use('/', express.static('static'))
-app.use('/lib', express.static('node_modules/angular'))
+app.use('/lib', express.static('node_modules'))
