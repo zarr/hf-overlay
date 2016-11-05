ec5fb1fda3c1e81b79f9af7dc9a6aaf6b79d7571
Arto Seppa <zar@iki.fi>
2016-11-05 17:27:26 +0200

    Add another overlay element

--- static/code.js
+++ static/code.js
@@ -1,8 +1,27 @@
 angular.module('overlayApp', [])
     .controller('IntervieweeController', function() {
-        var intervieweeData = this;
-        intervieweeData.interviewee = {
+        var interviewee = this;
+        interviewee.interviewee = {
             name: "Mikko",
             title: "Happy coder"
+        }
+        interviewee.enter = function () {
+            interviewee.in = !interviewee.in;
+        }
+    })
+    .controller('CasterController', function() {
+        var caster = this;
+        caster.caster1 = {
+            name: "Matti Mainio",
+            nick: "MasaHyper",
+            tag: "#masahyper"
+        }
+        caster.caster2 = {
+            name: "Miisa Mainio",
+            nick: "MisseSuper",
+            tag: "#missesuper"
+        }
+        caster.enter = function () {
+            caster.in = !caster.in;
         }
     });
