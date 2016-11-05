a5ee157e0b95f742118462cbc957349abdf17282
Arto Seppa <zar@iki.fi>
2016-11-05 18:07:49 +0200

    Simple integration with socket.io

--- static/code.js
+++ static/code.js
@@ -1,15 +1,20 @@
 angular.module('overlayApp', [])
-    .controller('IntervieweeController', function() {
+    .controller('IntervieweeController', function(socket) {
         var interviewee = this;
         interviewee.interviewee = {
             name: "Mikko",
             title: "Happy coder"
-        }
+        };
         interviewee.enter = function () {
             interviewee.in = !interviewee.in;
-        }
+        };
+        socket.on('message', function (msg) {
+            if ("interview" == msg) {
+                interviewee.enter();
+            }
+        })
     })
-    .controller('CasterController', function() {
+    .controller('CasterController', function(socket) {
         var caster = this;
         caster.caster1 = {
             name: "Matti Mainio",
@@ -24,4 +29,32 @@ angular.module('overlayApp', [])
         caster.enter = function () {
             caster.in = !caster.in;
         }
-    });
+        socket.on('message', function (msg) {
+            if ("casters" == msg) {
+                caster.enter();
+            }
+        })
+    })
+    .factory('socket', function ($rootScope) {
+        var socket = io.connect();
+        return {
+            on: function (eventName, callback) {
+                socket.on(eventName, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        callback.apply(socket, args);
+                    });
+                });
+            },
+            emit: function (eventName, data, callback) {
+                socket.emit(eventName, data, function () {
+                    var args = arguments;
+                    $rootScope.$apply(function () {
+                        if (callback) {
+                            callback.apply(socket, args);
+                        }
+                    });
+                })
+            }
+        };
+});
